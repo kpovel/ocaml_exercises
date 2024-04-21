829d14ae69531832f6f149a0395a1d15c0d70a7b
Pavlo Karas <thepavlokaras@gmail.com>
2024-04-21 18:03:42 +0300

    I didn't know I could do that with `as`

--- bin/main.ml
+++ bin/main.ml
@@ -1,20 +1,26 @@
-let encode l =
-  let mapped = List.map (fun x -> (1, x)) l in
-  List.rev
-    (List.fold_left
-       (fun acc (r, c) ->
-         match acc with
-         | [] -> [ (r, c) ]
-         | (count, last_char) :: list ->
-             if last_char = c then (count + 1, last_char) :: list
-             else (r, c) :: (count, last_char) :: list)
-       [] mapped)
+let encode list =
+  let rec aux count acc = function
+    | [] -> []
+    | [ x ] -> (count + 1, x) :: acc
+    | a :: (b :: _unused as t) ->
+        (* Printf.printf "A: %s, B: %s\n" a b; *)
+        Printf.printf "Unused list: ";
+        List.iter (fun x -> Printf.printf "%s, " x) _unused;
+        Printf.printf "\nUsed list:   ";
+        List.iter (fun x -> Printf.printf "%s, " x) t;
+        Printf.printf "\n";
+        if a = b then aux (count + 1) acc t else aux 0 ((count + 1, a) :: acc) t
+  in
+  List.rev (aux 0 [] list)
 
 let () =
-  let result =
-    encode
-      [ "a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e" ]
+  Printf.printf "\n";
+  let input =
+    [ "a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e" ]
   in
+
+  let result = encode input in
+
   List.iter
     (fun (count, string) ->
       Printf.printf "Count: %d, String: %s\n" count string)
