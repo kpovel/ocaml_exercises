8d4314f587ba82552db62cff3a8260dbd4023479
Pavlo Karas <thepavlokaras@gmail.com>
2024-04-21 20:58:02 +0300

    Modified Run-Length Encoding

--- bin/main.ml
+++ bin/main.ml
@@ -1,17 +1,19 @@
+type 'a rle = One of 'a | Many of int * 'a
+
 let encode list =
-  let rec aux count acc = function
+  let rec aux count acc list =
+    match list with
     | [] -> []
     | [ x ] -> (count + 1, x) :: acc
-    | a :: (b :: _unused as t) ->
-        (* Printf.printf "A: %s, B: %s\n" a b; *)
-        Printf.printf "Unused list: ";
-        List.iter (fun x -> Printf.printf "%s, " x) _unused;
-        Printf.printf "\nUsed list:   ";
-        List.iter (fun x -> Printf.printf "%s, " x) t;
-        Printf.printf "\n";
-        if a = b then aux (count + 1) acc t else aux 0 ((count + 1, a) :: acc) t
+    | a :: (b :: _ as t) -> (
+        match a = b with
+        | true -> aux (count + 1) acc t
+        | false -> aux 0 ((count + 1, a) :: acc) t)
   in
-  List.rev (aux 0 [] list)
+  let list = List.rev (aux 0 [] list) in
+  List.map
+    (fun (count, str) -> if count = 1 then One str else Many (count, str))
+    list
 
 let () =
   Printf.printf "\n";
@@ -22,6 +24,8 @@ let () =
   let result = encode input in
 
   List.iter
-    (fun (count, string) ->
-      Printf.printf "Count: %d, String: %s\n" count string)
+    (fun el ->
+      match el with
+      | One str -> Printf.printf "Got one element of %s\n" str
+      | Many (count, str) -> Printf.printf "Got %n elements of %s\n" count str)
     result
