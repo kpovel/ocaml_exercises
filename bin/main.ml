0fc7ab4abb44fe7dc8230016d53e47265180207d
Pavlo Karas <thepavlokaras@gmail.com>
2024-04-21 15:26:14 +0300

    feat: Run-Length Encoding

--- bin/main.ml
+++ bin/main.ml
@@ -1,7 +1,21 @@
-let () =
-  let s = new Exercises.Classes.double_stack [ 4; 2; 0 ] in
+let encode l =
+  let mapped = List.map (fun x -> (1, x)) l in
+  List.rev
+    (List.fold_left
+       (fun acc (r, c) ->
+         match acc with
+         | [] -> [ (r, c) ]
+         | (count, last_char) :: list ->
+             if last_char = c then (count + 1, last_char) :: list
+             else (r, c) :: (count, last_char) :: list)
+       [] mapped)
 
-  s#push 69;
-  s#print;
-  let _ = s#pop in
-  s#print
+let () =
+  let result =
+    encode
+      [ "a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e" ]
+  in
+  List.iter
+    (fun (count, string) ->
+      Printf.printf "Count: %d, String: %s\n" count string)
+    result
