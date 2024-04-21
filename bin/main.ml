58ad9854f970e36804b073ba4fbc0eb275c08c66
Pavlo Karas <thepavlokaras@gmail.com>
2024-04-21 21:07:23 +0300

    Duplicate the Elements of a List

--- bin/main.ml
+++ bin/main.ml
@@ -1,31 +1,7 @@
-type 'a rle = One of 'a | Many of int * 'a
-
-let encode list =
-  let rec aux count acc list =
-    match list with
-    | [] -> []
-    | [ x ] -> (count + 1, x) :: acc
-    | a :: (b :: _ as t) -> (
-        match a = b with
-        | true -> aux (count + 1) acc t
-        | false -> aux 0 ((count + 1, a) :: acc) t)
-  in
-  let list = List.rev (aux 0 [] list) in
-  List.map
-    (fun (count, str) -> if count = 1 then One str else Many (count, str))
-    list
+let rec duplicate list =
+  match list with [] -> [] | h :: t -> h :: h :: duplicate t
 
 let () =
   Printf.printf "\n";
-  let input =
-    [ "a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e" ]
-  in
-
-  let result = encode input in
-
-  List.iter
-    (fun el ->
-      match el with
-      | One str -> Printf.printf "Got one element of %s\n" str
-      | Many (count, str) -> Printf.printf "Got %n elements of %s\n" count str)
-    result
+  let result = duplicate [ "a"; "b"; "c"; "c"; "d" ] in
+  Printf.printf "%s" (String.concat ", " result)
