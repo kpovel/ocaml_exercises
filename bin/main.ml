66550b30f6feb8089738d11b148ad05755c5b4c9
Pavlo Karas <thepavlokaras@gmail.com>
2024-04-21 08:27:14 +0300

    class me daddy

--- bin/main.ml
+++ bin/main.ml
@@ -1,26 +1,55 @@
-let rec without_last list =
-  match list with
-  | [] | [ _ ] -> []
-  | first :: list -> first :: without_last list
+type 'a iterator = < get : 'a ; has_value : bool ; next : unit >
 
-let rec is_palindrome list =
-  match list with
-  | [] -> true
-  | [ _ ] -> true
-  | [ x; y ] -> x = y
-  | first :: list -> (
-      let last = List.nth list (List.length list - 1) in
-      match first = last with
-      | true ->
-          let without_last = without_last list in
-          is_palindrome without_last
-      | false -> false)
+class ['a] list_iterator init =
+  object
+    val mutable current : 'a list = init
+    method has_value = not (List.is_empty current)
+
+    method get =
+      match current with
+      | hd :: _tl -> hd
+      | [] -> raise (Invalid_argument "no value")
+
+    method next =
+      match current with
+      | _hd :: tl -> current <- tl
+      | [] -> raise (Invalid_argument "no value")
+  end
+
+class ['a] istack init =
+  object
+    val mutable v : 'a list = init
+
+    method pop =
+      match v with
+      | hd :: tl ->
+          v <- tl;
+          Some hd
+      | [] -> None
+
+    method v = v
+    method push hd = v <- hd :: v
+    method iterator : 'a iterator = new list_iterator v
+  end
 
 let () =
   Printf.printf "\n";
 
-  let list = [ "x"; "a"; "m"; "a"; "x" ] in
-  let result = is_palindrome list in
-  Printf.printf "%B\n" result;
-  let result = is_palindrome [ "a"; "b" ] in
-  Printf.printf "%B\n" result
+  let s = new istack [ 4; 2; 0 ] in
+
+  s#push 69;
+  let _ = s#pop in
+  let _ = s#pop in
+
+  let it = s#iterator in
+  let _ = it#get in
+
+  it#next;
+  let value = it#get in
+  print_int value;
+
+  it#next;
+
+  let string_list = List.map (fun x -> Int.to_string x) s#v in
+  let formated = String.concat "; " string_list in
+  Printf.printf "[%s]\n" formated
