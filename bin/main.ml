1ba73ab9f2e22cb7843a3ecb44aaf70a742fbae8
Pavlo Karas <thepavlokaras@gmail.com>
2024-04-21 06:44:10 +0300

    feat: is palindrome

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,26 @@
-let () = print_endline "Hello, World!"
+let rec without_last list =
+  match list with
+  | [] | [ _ ] -> []
+  | first :: list -> first :: without_last list
+
+let rec is_palindrome list =
+  match list with
+  | [] -> true
+  | [ _ ] -> true
+  | [ x; y ] -> x = y
+  | first :: list -> (
+      let last = List.nth list (List.length list - 1) in
+      match first = last with
+      | true ->
+          let without_last = without_last list in
+          is_palindrome without_last
+      | false -> false)
+
+let () =
+  Printf.printf "\n";
+
+  let list = [ "x"; "a"; "m"; "a"; "x" ] in
+  let result = is_palindrome list in
+  Printf.printf "%B\n" result;
+  let result = is_palindrome [ "a"; "b" ] in
+  Printf.printf "%B\n" result
