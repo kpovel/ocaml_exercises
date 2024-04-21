9f65f0e538c3e8d013c58eaf803c37cf90c3a50a
Pavlo Karas <thepavlokaras@gmail.com>
2024-04-21 12:38:57 +0300

    refactor: put classes into modules

--- bin/main.ml
+++ bin/main.ml
@@ -1,38 +1,5 @@
-open Base
-
-class ['a] stack init =
-  object
-    val mutable v : 'a list = init
-
-    method pop =
-      match v with
-      | hd :: tl ->
-          v <- tl;
-          Some hd
-      | [] -> None
-
-    method v = v
-    method push hd = v <- hd :: v
-    method iter f = List.iter ~f v
-  end
-
-class ['a] sstack init =
-  object
-    inherit ['a] stack init
-
-    method print =
-      List.iter ~f:(Stdio.printf "%d, ") v;
-      Stdlib.print_string "\n"
-  end
-
-class double_stack init =
-  object
-    inherit [int] sstack init as super
-    method! push hd = super#push (hd * 2)
-  end
-
 let () =
-  let s = new double_stack [ 4; 2; 0 ] in
+  let s = new Exercises.Classes.double_stack [ 4; 2; 0 ] in
 
   s#push 69;
   s#print;
