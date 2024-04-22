40ae20761b91ea83c1b527463781741a3277c0e6
Pavlo Karas <thepavlokaras@gmail.com>
2024-04-22 07:59:38 +0300

    Split a List Into Two Parts; The Length of the First Part Is Given

--- bin/main.ml
+++ bin/main.ml
@@ -1,7 +1,18 @@
-let rec duplicate list =
-  match list with [] -> [] | h :: t -> h :: h :: duplicate t
+let split list position =
+  let rec aux list position current_position =
+    match list with
+    | [] -> ([], [])
+    | h :: t ->
+        if position > current_position then
+          let x, y = aux t position (current_position + 1) in
+          (h :: x, y)
+        else ([ h ], t)
+  in
+  aux list position 1
 
 let () =
   Printf.printf "\n";
-  let result = duplicate [ "a"; "b"; "c"; "c"; "d" ] in
-  Printf.printf "%s" (String.concat ", " result)
+  let x, y = split [ "a"; "b"; "c"; "d"; "e"; "f"; "g"; "h"; "i"; "j" ] 3 in
+  Printf.printf "%s" (String.concat ", " x);
+  Printf.printf "\n";
+  Printf.printf "%s" (String.concat ", " y)
