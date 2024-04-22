5bd6846ca5b7e89f3a38b00206775a0b07e3e15d
Pavlo Karas <thepavlokaras@gmail.com>
2024-04-22 12:39:56 +0300

    Lotto: Draw N Different Random Numbers From the Set 1..M

--- bin/main.ml
+++ bin/main.ml
@@ -1,8 +1,10 @@
-let rec range i n =
-  match i <= n with true -> i :: range (i + 1) n | false -> []
+let rec lotto_select n m =
+  match n > 1 with
+  | true -> Random.int m :: lotto_select (n - 1) m
+  | false -> [ Random.int m ]
 
 let () =
   Printf.printf "\n";
-  let result = range 4 9 in
+  let result = lotto_select 6 49 in
   List.iter (fun x -> Printf.printf "%d, " x) result;
   Printf.printf "\n"
