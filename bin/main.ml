ac4ee1b1bed691bef061c3c563c7234157abde63
Pavlo Karas <thepavlokaras@gmail.com>
2024-05-04 06:58:54 +0300

    feat: insert sort

--- bin/main.ml
+++ bin/main.ml
@@ -1,7 +1,17 @@
-let isconsonant char =
-  match char with 'a' | 'e' | 'i' | 'o' | 'u' -> true | _ -> false
+let rec insert_sort x l =
+  match l with
+  | [] -> [ x ]
+  | hd :: tl -> if x <= hd then x :: hd :: tl else hd :: insert_sort x tl
+;;
+
+let rec sort l =
+  match l with
+  | [] -> []
+  | hd :: tl -> insert_sort hd @@ sort tl
+;;
 
 let () =
-  let result = isconsonant 'l' in
-  Printf.printf "%b" result;
+  let result = sort [ 6; 9; 4; 2; 0 ] in
+  List.iter (fun x -> Printf.printf "%d" x) result;
   Printf.printf "\n"
+;;
