6bbbb6bc59bf186b17501856b6ba6dcb43808a36
Pavlo Karas <thepavlokaras@gmail.com>
2024-05-05 22:16:13 +0300

    feat: mapl

--- bin/main.ml
+++ bin/main.ml
@@ -1,46 +1,16 @@
-let rec take n l =
-  match n = 0 with
-  | true -> []
-  | false ->
-    (match l with
-     | [] -> []
-     | hd :: tl -> hd :: take (n - 1) tl)
-;;
-
-let rec drop n l =
-  match n = 0 with
-  | true -> l
-  | false ->
-    (match l with
-     | [] -> []
-     | _ :: t -> drop (n - 1) t)
-;;
-
-let rec merge x y =
-  match x, y with
-  | [], l -> l
-  | l, [] -> l
-  | hx :: tx, hy :: ty ->
-    (match hx < hy with
-     | true -> hx :: merge tx (hy :: ty)
-     | false -> hy :: merge (hx :: tx) ty)
-;;
-
-let rec msort l =
+let rec map f l =
   match l with
   | [] -> []
-  | [ x ] -> [ x ]
-  | _ ->
-    let half_len = List.length l / 2 in
-    let left = take half_len l in
-    let right = drop half_len l in
-    merge (msort left) (msort right)
+  | hd :: tl -> f hd :: map f tl
+;;
+
+let rec mapl f l =
+  match l with
+  | [] -> []
+  | hd :: tl -> map f hd :: mapl f tl
 ;;
 
 let () =
-  msort [ [ 'o'; 'n'; 'e' ]; [ 't'; 'w'; 'o' ]; [ 't'; 'h'; 'r'; 'e'; 'e' ] ]
-  |> List.sort (fun x y -> Stdlib.compare x y)
-  |> List.map (fun x -> String.concat ";" (List.map (String.make 1) x))
-  |> List.iter (fun x -> Stdio.printf "[%s]," x)
-in
-Stdio.printf "\n"
+  let _ = mapl (fun x -> x * 2) [ [ 4 ]; [ 2 ]; [ 0 ]; [ 6; 9 ] ] in
+  Stdio.printf "\n"
+;;
