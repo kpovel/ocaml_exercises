68b4e05d0c90bd6ba0d729654098f350d67f8ae7
Pavlo Karas <thepavlokaras@gmail.com>
2024-05-08 11:06:38 +0300

    feat: lookup

--- bin/main.ml
+++ bin/main.ml
@@ -1,16 +1,44 @@
-let rec map f l =
+open Base
+
+let fst (x, _) = x
+let snt (_, y) = y
+
+let rec lookup x l =
   match l with
-  | [] -> []
-  | hd :: tl -> f hd :: map f tl
+  | [] -> None
+  | (k, v) :: tl ->
+    (match k = x with
+     | true -> Some v
+     | false -> lookup x tl)
 ;;
 
-let rec mapl f l =
+let rec add k v l =
+  match l with
+  | [] -> [ k, v ]
+  | (k', v') :: tl ->
+    (match k' = k with
+     | true -> (k, v) :: tl
+     | false -> (k', v') :: add k v l)
+;;
+
+let rec remove k l =
   match l with
   | [] -> []
-  | hd :: tl -> map f hd :: mapl f tl
+  | (k', v') :: tl ->
+    (match k' = k with
+     | true -> tl
+     | false -> (k', v') :: remove k tl)
+;;
+
+let key_exists k l =
+  match lookup k l with
+  | Some _ -> true
+  | None -> false
 ;;
 
 let () =
-  let _ = mapl (fun x -> x * 2) [ [ 4 ]; [ 2 ]; [ 0 ]; [ 6; 9 ] ] in
-  Stdio.printf "\n"
+  let census = [ 1, 4; 2, 2; 3, 2; 4, 3; 5, 1; 6, 2 ] in
+  match lookup 4 census with
+  | Some vl -> Stdlib.Printf.printf "We god em %d" vl
+  | None -> Stdlib.Printf.printf "Noting found"
 ;;
