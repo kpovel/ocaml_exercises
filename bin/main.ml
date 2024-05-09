c63244631308954e4af7cebf0d69aa7993dffaac
Pavlo Karas <thepavlokaras@gmail.com>
2024-05-09 21:51:27 +0300

    feat: dict

--- bin/main.ml
+++ bin/main.ml
@@ -1,4 +1,4 @@
-open Base
+open Core
 
 let fst (x, _) = x
 let snt (_, y) = y
@@ -36,9 +36,61 @@ let key_exists k l =
   | None -> false
 ;;
 
+let n_kens l =
+  let rec acc count l =
+    match l with
+    | [] -> count
+    | _ :: tl -> acc (count + 1) tl
+  in
+  acc 0 l
+;;
+
+let rec replace k v l =
+  match l with
+  | [] -> raise_s (Sexp.Atom "Not_found")
+  | (k', v') :: tl ->
+    (match k' = k with
+     | true -> (k, v) :: tl
+     | false -> (k', v') :: replace k v tl)
+;;
+
+let rec mkdict keys values =
+  match keys, values with
+  | [], [] -> []
+  | [], _ | _, [] -> raise_s (Sexp.Atom "Invalid_argument")
+  | k :: ks, v :: vs -> (k, v) :: mkdict ks vs
+;;
+
+let rec mklists dict =
+  match dict with
+  | [] -> [], []
+  | (k, v) :: hd ->
+    (match mklists hd with
+     | ks, vs -> k :: ks, v :: vs)
+;;
+
+let rec todict l =
+  match l with
+  | [] -> []
+  | (k, v) :: tl ->
+    let acc = todict tl in
+    (match key_exists k acc with
+     | true -> acc
+     | false -> (k, v) :: acc)
+;;
+
+let rec union a b =
+  match b with
+  | [] -> a
+  | (k, v) :: tl ->
+    let result = union a tl in
+    (match key_exists k a with
+     | true -> result
+     | false -> (k, v) :: result)
+;;
+
 let () =
-  let census = [ 1, 4; 2, 2; 3, 2; 4, 3; 5, 1; 6, 2 ] in
-  match lookup 4 census with
-  | Some vl -> Stdlib.Printf.printf "We god em %d" vl
-  | None -> Stdlib.Printf.printf "Noting found"
+  let census = union [ 1, 1; 2, 2; 3, 3 ] [ 4, 4; 1, 2 ] in
+  List.iter census ~f:(fun (k, v) -> Stdlib.Printf.printf "Key: %d, Value: %d\n" k v);
+  ()
 ;;
