fda7c4f8d2338a77da10a85c5e456dd4237817b8
Pavlo Karas <thepavlokaras@gmail.com>
2024-05-15 08:16:08 +0300

    feat: More with Functions

--- bin/main.ml
+++ bin/main.ml
@@ -1,96 +1,40 @@
 open Core
 
-let fst (x, _) = x
-let snt (_, y) = y
-
-let rec lookup x l =
-  match l with
-  | [] -> None
-  | (k, v) :: tl ->
-    (match k = x with
-     | true -> Some v
-     | false -> lookup x tl)
-;;
-
-let rec add k v l =
-  match l with
-  | [] -> [ k, v ]
-  | (k', v') :: tl ->
-    (match k' = k with
-     | true -> (k, v) :: tl
-     | false -> (k', v') :: add k v l)
-;;
-
-let rec remove k l =
+let rec map ~f l =
   match l with
   | [] -> []
-  | (k', v') :: tl ->
-    (match k' = k with
-     | true -> tl
-     | false -> (k', v') :: remove k tl)
+  | hd :: tl -> f hd :: map ~f tl
 ;;
 
-let key_exists k l =
-  match lookup k l with
-  | Some _ -> true
-  | None -> false
+let rec member x l =
+  match l with
+  | [] -> false
+  | hd :: tl -> hd = x || member x tl
 ;;
 
-let n_kens l =
-  let rec acc count l =
-    match l with
-    | [] -> count
-    | _ :: tl -> acc (count + 1) tl
+let member_all x ls = List.map ls ~f:(member x) |> List.exists ~f:not |> not
+let mapll ~f l = map ~f:(map ~f:(map ~f)) l
+
+let truncate len ll =
+  let rec trunc len list =
+    match list with
+    | [] -> []
+    | hd :: tl -> if len <= 1 then [ hd ] else hd :: trunc (len - 1) tl
   in
-  acc 0 l
+  map ~f:(trunc len) ll
 ;;
 
-let rec replace k v l =
-  match l with
-  | [] -> raise_s (Sexp.Atom "Not_found")
-  | (k', v') :: tl ->
-    (match k' = k with
-     | true -> (k, v) :: tl
-     | false -> (k', v') :: replace k v tl)
-;;
-
-let rec mkdict keys values =
-  match keys, values with
-  | [], [] -> []
-  | [], _ | _, [] -> raise_s (Sexp.Atom "Invalid_argument")
-  | k :: ks, v :: vs -> (k, v) :: mkdict ks vs
-;;
-
-let rec mklists dict =
-  match dict with
-  | [] -> [], []
-  | (k, v) :: hd ->
-    (match mklists hd with
-     | ks, vs -> k :: ks, v :: vs)
-;;
-
-let rec todict l =
-  match l with
-  | [] -> []
-  | (k, v) :: tl ->
-    let acc = todict tl in
-    (match key_exists k acc with
-     | true -> acc
-     | false -> (k, v) :: acc)
-;;
-
-let rec union a b =
-  match b with
-  | [] -> a
-  | (k, v) :: tl ->
-    let result = union a tl in
-    (match key_exists k a with
-     | true -> result
-     | false -> (k, v) :: result)
+let rec firstelts n ll =
+  let first n l =
+    match l with
+    | [] -> n
+    | hd :: _ -> hd
+  in
+  map ~f:(first n) ll
 ;;
 
 let () =
-  let census = union [ 1, 1; 2, 2; 3, 3 ] [ 4, 4; 1, 2 ] in
-  List.iter census ~f:(fun (k, v) -> Stdlib.Printf.printf "Key: %d, Value: %d\n" k v);
+  firstelts 69 [ [ 4; 2; 0 ]; [ 10; 20; 30 ]; [] ]
+  |> List.iter ~f:(fun x -> Printf.printf "%d, " x);
   ()
 ;;
