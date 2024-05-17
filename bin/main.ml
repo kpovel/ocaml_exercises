8008553368f017f58cded1dbb3c440debbe631e3
Pavlo Karas <thepavlokaras@gmail.com>
2024-05-17 16:20:10 +0300

    feat: New Kinds of Data

--- bin/main.ml
+++ bin/main.ml
@@ -1,40 +1,89 @@
-open Core
+open Base
 
-let rec map ~f l =
-  match l with
-  | [] -> []
-  | hd :: tl -> f hd :: map ~f tl
-;;
+type color =
+  | Red
+  | Green
+  | Blue
+  | Yellow
+  | RGB of int * int * int
 
-let rec member x l =
-  match l with
-  | [] -> false
-  | hd :: tl -> hd = x || member x tl
-;;
+type 'a option =
+  | None
+  | Some of 'a
 
-let member_all x ls = List.map ls ~f:(member x) |> List.exists ~f:not |> not
-let mapll ~f l = map ~f:(map ~f:(map ~f)) l
+type expr =
+  | Num of int
+  | Add of expr * expr
+  | Substruct of expr * expr
+  | Multiply of expr * expr
+  | Divide of expr * expr
+  | Pow of expr * expr
 
-let truncate len ll =
-  let rec trunc len list =
-    match list with
-    | [] -> []
-    | hd :: tl -> if len <= 1 then [ hd ] else hd :: trunc (len - 1) tl
+let rec evaluate e =
+  let rec ev e =
+    match e with
+    | Num x -> x
+    | Add (e, e') -> ev e + ev e'
+    | Substruct (e, e') -> ev e - ev e'
+    | Multiply (e, e') -> ev e * ev e'
+    | Divide (e, e') -> ev e / ev e'
+    | Pow (e, e') -> ev e ** ev e'
   in
-  map ~f:(trunc len) ll
+  try Some (ev e) with
+  | Division_by_zero -> None
 ;;
 
-let rec firstelts n ll =
-  let first n l =
-    match l with
-    | [] -> n
-    | hd :: _ -> hd
-  in
-  map ~f:(first n) ll
+type rect =
+  | Rectangle of int * int
+  | Square of int
+
+let area rect =
+  match rect with
+  | Rectangle (w, l) -> w * l
+  | Square a -> a * a
 ;;
 
-let () =
-  firstelts 69 [ [ 4; 2; 0 ]; [ 10; 20; 30 ]; [] ]
-  |> List.iter ~f:(fun x -> Printf.printf "%d, " x);
-  ()
+let rotate rect =
+  match rect with
+  | Rectangle (w, l) -> if w > l then Rectangle (l, w) else rect
+  | rest -> rest
 ;;
+
+let pack rectl =
+  List.map rectl ~f:rotate
+  |> List.sort ~compare:(fun x y ->
+    match area x, area y with
+    | x, y when x < y -> -1
+    | x, y when x > y -> 1
+    | _ -> 0)
+;;
+
+type 'a sequence =
+  | Nil
+  | Cons of 'a * 'a sequence
+
+let rec take sequence n =
+  if n <= 0
+  then Nil
+  else (
+    match sequence with
+    | Nil -> Nil
+    | Cons (a, a') -> Cons (a, take a' (n - 1)))
+;;
+
+let rec drop sequence n =
+  if n <= 0
+  then sequence
+  else (
+    match sequence with
+    | Nil -> Nil
+    | Cons (_, a') -> drop a' (n - 1))
+;;
+
+let rec map f seq =
+  match seq with
+  | Nil -> Nil
+  | Cons (a, a') -> Cons (f a, map f a')
+;;
+
+let () = ()
