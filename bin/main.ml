35dbfc737be6691f0ae184fa94a92672d1d89a2a
Pavlo Karas <thepavlokaras@gmail.com>
2024-06-12 09:48:22 +0300

    feat: The Other Numbers

--- bin/main.ml
+++ bin/main.ml
@@ -1,72 +1,38 @@
+let round n =
+  let f = floor n in
+  match f +. 0.5 >= n with
+  | true -> ceil n
+  | false -> f
+;;
+
+let equidistan (x, y) (x', y') = (x +. x') /. 2., (y +. y') /. 2.
+
+let parts n =
+  let f = floor n in
+  let rest = n -. f in
+  f, rest
+;;
+
+let star n =
+  assert (n >= 0. && n <= 1.);
+  let position = n *. 50. |> round |> Float.to_int in
+  let open Core in
+  for x = 0 to 50 do
+    match position = x with
+    | true -> printf "*"
+    | false -> printf " "
+  done;
+  printf "\n"
+;;
+
+let plot f range step =
+  let pos = ref 0. in
+  while range >= !pos do
+    star (f !pos);
+    pos := !pos +. step
+  done
+;;
+
 open Core
 
-let print_histogram arr =
-  Array.iteri arr ~f:(fun i x ->
-    printf
-      "For character '%c' (character number %d) the count is %d.()\n"
-      (char_of_int i)
-      i
-      x)
-;;
-
-let channel_statistics ch =
-  let file = In_channel.input_all ch in
-  let lines = ref 0 in
-  let words = ref 0 in
-  let sentences = ref 0 in
-  let characters = ref 0 in
-  let histogram = Array.init 256 ~f:(fun _ -> 0) in
-  String.iter file ~f:(fun c ->
-    characters := !characters + 1;
-    let i = int_of_char c in
-    histogram.(i) <- histogram.(i) + 1;
-    match c with
-    | '\n' ->
-      lines := !lines + 1;
-      words := !words + 1
-    | '.' | '?' | '!' -> sentences := !sentences + 1
-    | ' ' -> words := !words + 1
-    | _ -> ());
-  printf
-    "There were %d lines, making up %d characters with %d words in %d sentences.\n"
-    !lines
-    !characters
-    !words
-    !sentences;
-  print_histogram histogram
-;;
-
-let rec forloop f t exp =
-  match f <= t with
-  | true ->
-    exp f;
-    forloop (f + 1) t exp
-  | false -> ()
-;;
-
-let sum arr =
-  let sum = ref 0 in
-  for i = 0 to Array.length arr - 1 do
-    sum := !sum + arr.(i)
-  done;
-  !sum
-;;
-
-let to_lowercase char =
-  let ascii = int_of_char char in
-  match ascii with
-  | x when x >= 65 && x <= 90 -> char_of_int (ascii + 32)
-  | _ -> char
-;;
-
-let to_uppercase char =
-  let ascii = int_of_char char in
-  match ascii with
-  | x when x >= 97 && x <= 122 -> char_of_int (ascii - 32)
-  | _ -> char
-;;
-
-let () = to_lowercase 'Z' |> printf "%c\n"
-(*let ch = In_channel.create "gregor.txt" in*)
-(*channel_statistics ch;*)
-(*In_channel.close ch*)
+let () = plot Float.sin Float.pi (Float.pi /. 20.)
