cacb02720d4f9f378c2db7bc5f45f071e35331df
Pavlo Karas <thepavlokaras@gmail.com>
2024-06-14 09:45:34 +0300

    feat: The OCaml Standard Library

--- bin/main.ml
+++ bin/main.ml
@@ -1,38 +1,40 @@
-let round n =
-  let f = floor n in
-  match f +. 0.5 >= n with
-  | true -> ceil n
-  | false -> f
+let rec concat l =
+  match l with
+  | [] -> []
+  | hd :: tl -> hd @ concat tl
 ;;
 
-let equidistan (x, y) (x', y') = (x +. x') /. 2., (y +. y') /. 2.
-
-let parts n =
-  let f = floor n in
-  let rest = n -. f in
-  f, rest
+let rec all_true l =
+  match l with
+  | [] -> true
+  | hd :: tl -> if not (List.mem true hd) then false else all_true tl
 ;;
 
-let star n =
-  assert (n >= 0. && n <= 1.);
-  let position = n *. 50. |> round |> Float.to_int in
-  let open Core in
-  for x = 0 to 50 do
-    match position = x with
-    | true -> printf "*"
-    | false -> printf " "
+let count_exclamation s = String.split_on_char '!' s |> List.length |> ( + ) (-1)
+
+let exclamation_to_period s =
+  String.map
+    (fun c ->
+      match c with
+      | '!' -> '.'
+      | n -> n)
+    s
+;;
+
+let str_concat l = String.concat "" l
+
+let buf_concat l =
+  let buf = Buffer.create 100 in
+  List.iter (Buffer.add_string buf) l;
+  Buffer.contents buf
+;;
+
+let count_ocamls s =
+  let count = ref 0 in
+  for i = 0 to String.length s - 5 do
+    if String.sub s i 5 |> String.starts_with ~prefix:"OCaml" then count := !count + 1
   done;
-  printf "\n"
+  !count
 ;;
 
-let plot f range step =
-  let pos = ref 0. in
-  while range >= !pos do
-    star (f !pos);
-    pos := !pos +. step
-  done
-;;
-
-open Core
-
-let () = plot Float.sin Float.pi (Float.pi /. 20.)
+let () = count_ocamls "OCamlmycaml" |> Stdlib.Printf.printf "%d \n"
